11783d648405f619b184444505b9de6ecf0272c6
Helena Rasche <hxr@hx42.org>
2022-03-02 12:11:52 +0100

    add new listeners

--- api/slack_listeners.py
+++ api/slack_listeners.py
@@ -25,14 +25,37 @@ def handle_app_mentions(logger, event, say):
 @app.command("/completed")
 def completed(ack, body, logger, say):
     ack()
-    logger.debug(body)
+    if 'text' not in body:
+        say(
+            text=f":warning: Please run this command with the histories you are using as proof of completing this tutorial. E.g. /completed https://usegalaxy.../u/your-user/h/your-history\n\nWe will check these histories before granting your certificate at the end of the course.",
+            ephemeral=True
+        )
+        return HttpResponse(status=200)
+
     try:
         q = Transcript(slack_user_id=body['user_id'], channel=body['channel_name'], proof=body['text'])
         q.save()
-        say("Saved!")
-
+        say("Saved this course to your transcript! Congrats! You can use the command /transcript to list your transcript at any time.")
     except Exception as e:
         logger.error(e)
-        say(f"Something went wrong! ({e}) We could not record your completion. Please contact <@U01F7TAQXNG>")
+        say(
+            text=f"Something went wrong! ({e}) We could not record your completion. Please contact <@U01F7TAQXNG>",
+            ephemeral=True
+        )
 
     return HttpResponse(status=200)
+
+
+@csrf_exempt
+@app.command("/transcript")
+def transcript(ack, body, say):
+    ack()
+    logger.debug(body)
+
+    results = Transcript.objects.filter(slack_user_id=body['user_id'])
+    output = [x.channel for x in results]
+
+    say(
+        text="We have recorded you completed the following modules: " + ', '.join(output),
+        ephemeral=True
+    )
