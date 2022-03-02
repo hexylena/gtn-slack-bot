919fd7d796e9b43bd2e727b533459307ac4c11ca
Helena Rasche <hxr@hx42.org>
2022-03-02 10:53:44 +0100

    init with django

---
+++ api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Transcript(models.Model):
+    slack_user_id = models.CharField(max_length=32)
+    time = models.DateTimeField(auto_now_add=True)
+    channel = models.CharField(max_length=32)
+    proof = models.TextField()
+
+
+class CertificateRequest(models.Model):
+    slack_user_id = models.CharField(max_length=32)
+    human_name = models.CharField(max_length=64)
+    time = models.DateTimeField(auto_now_add=True)
+    course = models.TextField()
+    approved = models.CharField(max_length=32)
