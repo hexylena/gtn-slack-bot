6ee2a9a0887b900068d0fa2527688a9993863166
Helena Rasche <hxr@hx42.org>
2022-03-02 11:20:40 +0100

    update for slack

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from slack_bolt.adapter.django import SlackRequestHandler
+from .slack_listeners import app
+
+handler = SlackRequestHandler(app=app)
+
+from django.http import HttpRequest
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
+@csrf_exempt
+def slack_events_handler(request: HttpRequest):
+    return handler.handle(request)
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path("", slack_events_handler, name="slack_events"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from slack_bolt import App
+import os
+
+app = App(
+    token=os.environ["SLACK_BOT_TOKEN"],
+    # signing_secret=os.environ["SLACK_SIGNING_SECRET"],
+    # disable eagerly verifying the given SLACK_BOT_TOKEN value
+    token_verification_enabled=False,
+)
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Привіт Світ! You're at the GTN Certificate Bot index.")
+
+# def completed(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    # try:
+        # msg = processCompleted(body['user_id'], body['channel_name'], body['text'])
+        # ack(msg)
+    # except Exception as e:
+        # logger(e)
+        # ack("Something went wrong! We could not record your completion. Please contact <@U01F7TAQXNG>")
