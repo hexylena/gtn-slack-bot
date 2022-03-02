b1ce51bd090b69afd2161764f8c948cd9fbe83d0
Helena Rasche <hxr@hx42.org>
2022-03-02 12:20:08 +0100

    try with ephemeral

--- api/slack_listeners.py
+++ api/slack_listeners.py
@@ -2,7 +2,47 @@ import logging
 from .models import Transcript
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 import os
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+videosjson = os.path.join(file_path, 'videos.json')
+with open(videosjson, 'r') as handle:
+    videos = json.load(handle)
+
+def convert_name(path):
+    path_parts = path.split('/')
+    if path_parts[0] == 'statistics':
+        path_parts[0] = 'machine_learning'
+    if path_parts[0] == 'sequence-analysis':
+        path_parts[0] = 'ngs'
+
+    if 'galaxy/intro' in path or path_parts[0] == 'introduction':
+        return 'galaxy-intro'
+    elif path_parts[0] in ('galaxy', 'community', 'webinar'):
+        return 'general'
+    elif path_parts[0] == 'course':
+        # These are specific to their events
+        return '-'.join(['event', path_parts[1].replace('welcome-', '')])
+    elif path_parts[0] == 'contributing':
+        return 'gtn'
+    elif 'tool-generators' in path_parts[1]:
+        return 'dev-toolfactory'
+    elif len(path_parts) > 2 and path_parts[2] in ('tutorial', 'slides'):
+        return "_".join(path_parts[0:2])
+    elif len(path_parts) > 2:
+        return "_".join(path_parts[0:2])
+    elif len(path_parts) == 2:
+        return "_".join(path_parts[0:2])
+    return None
+
+channel_mapping = {}
+for k, v in videos.items():
+    c = convert_name(k)
+    if c not in channel_mapping:
+        channel_mapping[c] = []
+
+    channel_mapping[c].append(k)
 
 from slack_bolt import App
 
@@ -21,26 +61,50 @@ def handle_app_mentions(logger, event, say):
     logger.info(event)
     say(f"Hi there, <@{event['user']}>")
 
+
+@csrf_exempt
+@app.command("/debug")
+def debug(ack, body, logger, say):
+    ack()
+    say(f"{body}")
+
+
 @csrf_exempt
 @app.command("/completed")
-def completed(ack, body, logger, say):
+def completed(ack, body, logger, say, client):
     ack()
     if 'text' not in body:
-        say(
+        client.chat_postEphemeral(
+            channel=body['channel_id'],
+            user=body['user_id'],
             text=f":warning: Please run this command with the histories you are using as proof of completing this tutorial. E.g. /completed https://usegalaxy.../u/your-user/h/your-history\n\nWe will check these histories before granting your certificate at the end of the course.",
-            ephemeral=True
+        )
+        return HttpResponse(status=200)
+
+    if body['channel_name'] not in channel_mapping:
+        client.chat_postEphemeral(
+            channel=body['channel_id'],
+            user=body['user_id'],
+            text=f"This channel is not associated with a course module. If you believe this is an error, please contact <@U01F7TAQXNG>"
         )
         return HttpResponse(status=200)
 
     try:
         q = Transcript(slack_user_id=body['user_id'], channel=body['channel_name'], proof=body['text'])
         q.save()
-        say("Saved this course to your transcript! Congrats! You can use the command /transcript to list your transcript at any time.")
+        client.chat_postEphemeral(
+            channel=body['channel_id'],
+            user=body['user_id'],
+            text=f"Saved this course to your transcript! Congrats! You can use the command /transcript to list your transcript at any time."
+        )
+        return HttpResponse(status=200)
+
     except Exception as e:
         logger.error(e)
-        say(
-            text=f"Something went wrong! ({e}) We could not record your completion. Please contact <@U01F7TAQXNG>",
-            ephemeral=True
+        client.chat_postEphemeral(
+            channel=body['channel_id'],
+            user=body['user_id'],
+            text=f"Something went wrong! ({e}) We could not record your completion. Please contact <@U01F7TAQXNG>"
         )
 
     return HttpResponse(status=200)
@@ -48,14 +112,19 @@ def completed(ack, body, logger, say):
 
 @csrf_exempt
 @app.command("/transcript")
-def transcript(ack, body, say):
+def transcript(ack, body, client):
     ack()
     logger.debug(body)
 
     results = Transcript.objects.filter(slack_user_id=body['user_id'])
     output = [x.channel for x in results]
 
-    say(
-        text="We have recorded you completed the following modules: " + ', '.join(output),
-        ephemeral=True
+    # say(
+        # text=",
+        # ephemeral=True
+    # )
+    client.chat_postEphemeral(
+        channel=body['channel_id'],
+        user=body['user_id'],
+        text=f"We have recorded you completed the following modules: {', '.join(output)}"
     )
