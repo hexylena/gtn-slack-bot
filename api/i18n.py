3ef13f707a657a1312cec3364f40dca0402bf401
Helena Rasche <hxr@hx42.org>
2022-03-14 16:47:28 +0100

    move into separate folder

---
+++ api/i18n.py
@@ -0,0 +1,44 @@
+ENCOURAGEMENT = [
+    "Congratulations!",
+    "¡Buen trabajo!",
+    "¡Felicitaciones!",
+    "Fantastic work!",
+    "Great job!",
+    "太好了！",
+    "Gefeliciteerd!",
+    "чудова робота",
+    "Amazing!",
+    "Awesome!",
+    "Félicitations!",
+    "Bien fait!",
+    "Ben fatto!",
+    "bon travail",
+    "Wayɛ Adeɛ", # Twi from Ghana
+    "Mmo", # Twi
+    "शाबाश", # Hindi
+    "வாழ்த்துக்கள்", # Tamil
+    "ଅଭିନନ୍ଦନ ", # Odia,
+    "अभिनंदन", # Marathi
+    "ಅಭಿನಂದನೆಗಳು", # Kannada
+    "সাবাশ", # Bangla
+    "অভিনন্দন", # Assamese
+    "అభినందనలు", # Telugu
+    "അഭിനന്ദനങ്ങൾ", # Malayalam
+    "ਬਹੁਤ ਖੂਬ", # Punjabi
+    "શાબ્બાશ", # Gujarathi
+    "অভিনন্দন", # Bengali - Congratulations
+    "খুব ভালো", # Bengali - Very good
+    "Parabéns", # PT
+    "Bom trabalho", #PT
+    "Tebrikler", # Turkish
+    "Çok iyi gidiyorsun", # Turkish
+    "Bien hecho!", # ES
+    "Lo has logrado!", # ES
+    "እንኳን ደስ አሎት!", # Arahmaic
+    "Συγχαρητήρια", # Greek (Congrats!)
+    "Καλή δουλειά", # Greek (good job!)
+    "Ini tchié, bara gnana", # Bambara, local language spoken in Mali
+    "ਵਧਾਈਆਂ", # Punjabi
+    "धेरै राम्रो ", # Nepali
+    "Ekú oríré", # (Yoruba, Nigeria). It means 'Congratulations'
+]
--- api/slack_listeners.py
+++ api/slack_listeners.py
@@ -12,53 +12,10 @@ from .models import Transcript, CertificateRequest
 from django.http import HttpResponse
 from .videolibrary import CHANNEL_MAPPING, channel2module, validateGalaxyURLs, addCertificateRequest
 from django.views.decorators.csrf import csrf_exempt
+from .i18n import ENCOURAGEMENT
 import json
 import os
 
-ENCOURAGEMENT = [
-    "Congratulations!",
-    "¡Buen trabajo!",
-    "¡Felicitaciones!",
-    "Fantastic work!",
-    "Great job!",
-    "太好了！",
-    "Gefeliciteerd!",
-    "чудова робота",
-    "Amazing!",
-    "Awesome!",
-    "Félicitations!",
-    "Bien fait!",
-    "Ben fatto!",
-    "bon travail",
-    "Wayɛ Adeɛ", # Twi from Ghana
-    "Mmo", # Twi
-    "शाबाश", # Hindi
-    "வாழ்த்துக்கள்", # Tamil
-    "ଅଭିନନ୍ଦନ ", # Odia,
-    "अभिनंदन", # Marathi
-    "ಅಭಿನಂದನೆಗಳು", # Kannada
-    "সাবাশ", # Bangla
-    "অভিনন্দন", # Assamese
-    "అభినందనలు", # Telugu
-    "അഭിനന്ദനങ്ങൾ", # Malayalam
-    "ਬਹੁਤ ਖੂਬ", # Punjabi
-    "શાબ્બાશ", # Gujarathi
-    "অভিনন্দন", # Bengali - Congratulations
-    "খুব ভালো", # Bengali - Very good
-    "Parabéns", # PT
-    "Bom trabalho", #PT
-    "Tebrikler", # Turkish
-    "Çok iyi gidiyorsun", # Turkish
-    "Bien hecho!", # ES
-    "Lo has logrado!", # ES
-    "እንኳን ደስ አሎት!", # Arahmaic
-    "Συγχαρητήρια", # Greek (Congrats!)
-    "Καλή δουλειά", # Greek (good job!)
-    "Ini tchié, bara gnana", # Bambara, local language spoken in Mali
-    "ਵਧਾਈਆਂ", # Punjabi
-    "धेरै राम्रो ", # Nepali
-    "Ekú oríré", # (Yoruba, Nigeria). It means 'Congratulations'
-]
 
 print(ENCOURAGEMENT)
 
