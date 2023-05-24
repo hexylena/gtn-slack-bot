1f5bbc453dcbf8a5337836742da225b30a073c5c
Helena Rasche <hexylena@galaxians.org>
2023-05-24 13:13:24 +0200

    copy gratitude to the channel

--- api/slack_listeners.py
+++ api/slack_listeners.py
@@ -151,9 +151,16 @@ def handle_messages(event, logger):
 
 
 @app.event("reaction_added")
-def handle_reactions(event, logger):
-    print(event)
-    logger.info(event)
+def handle_reactions(client, event, logger):
+    #{'type': 'reaction_added', 'user': 'U058SE9GY8P', 'reaction': 'yellow_heart', 'item': {'type': 'message', 'channel': 'C01ES8R0RNG', 'ts': '1684920076.690419'}, 'item_user': 'U01EDBVM04W', 'event_ts': '1684926148.066100'}
+    if 'gratitude-' in event['reaction']:
+        logger.info(event)
+        url = f"https://gtnsmrgsbord.slack.com/archives/#{event['item']['channel']}/p#{event['item']['channel'].replace('.', '')}"
+        message(
+            client,
+            "#gratitude",
+            url
+        )
 
 
 @app.event("app_mention")
